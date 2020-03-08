b45452d9e7eeb5357673dc0355dea6e83e83af30
Alexander Makarov <sam@rmcreative.ru>
2020-03-08 19:45:34 +0000

    Apply fixes from StyleCI

--- tests/QueryBuilderTest.php
+++ tests/QueryBuilderTest.php
@@ -215,7 +215,7 @@ class QueryBuilderTest extends AbstractQueryBuilderTest
         $db = $this->getConnection();
 
         $expectedQuerySql = $this->replaceQuotes(
-             'WITH a1 AS (SELECT [[id]] FROM [[t1]] WHERE expr = 1), a2 AS (SELECT [[id]] FROM [[t2]] INNER JOIN [[a1]] ON t2.id = a1.id WHERE expr = 2 UNION  SELECT [[id]] FROM [[t3]] WHERE expr = 3) SELECT * FROM [[a2]]'
+            'WITH a1 AS (SELECT [[id]] FROM [[t1]] WHERE expr = 1), a2 AS (SELECT [[id]] FROM [[t2]] INNER JOIN [[a1]] ON t2.id = a1.id WHERE expr = 2 UNION  SELECT [[id]] FROM [[t3]] WHERE expr = 3) SELECT * FROM [[a2]]'
         );
 
         $with1Query = (new Query($db))
