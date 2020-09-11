2bb5d2e68239d6ae9aca2662feeae0db983ef79d
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2020-09-11 14:43:20 -0300

    Add getDriverName() method in Connection::class. (#31)

--- src/Connection.php
+++ src/Connection.php
@@ -97,4 +97,14 @@ final class Connection extends AbstractConnection
             }
         }
     }
+
+    /**
+     * Returns the name of the DB driver.
+     *
+     * @return string name of the DB driver
+     */
+    public function getDriverName(): string
+    {
+        return 'sqlite';
+    }
 }
