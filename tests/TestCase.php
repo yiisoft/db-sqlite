b4e0bf63774a5a0050f85ee44517063410225120
Rustam Mamadaminov <rmamdaminov@gmail.com>
2020-12-31 16:01:14 +0500

    Fix tests for yiisoft/db#209 (#62)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -25,6 +25,7 @@ use Yiisoft\Di\Container;
 use Yiisoft\Factory\Definitions\Reference;
 use Yiisoft\Log\Logger;
 use Yiisoft\Profiler\Profiler;
+use Yiisoft\Profiler\ProfilerInterface;
 
 use function explode;
 use function file_get_contents;
@@ -41,7 +42,7 @@ class TestCase extends AbstractTestCase
     protected string $likeEscapeCharSql = '';
     protected array $likeParameterReplacements = [];
     protected LoggerInterface $logger;
-    protected Profiler $profiler;
+    protected ProfilerInterface $profiler;
 
     protected function setUp(): void
     {
@@ -110,7 +111,7 @@ class TestCase extends AbstractTestCase
         $this->aliases = $this->container->get(Aliases::class);
         $this->cache = $this->container->get(CacheInterface::class);
         $this->logger = $this->container->get(LoggerInterface::class);
-        $this->profiler = $this->container->get(Profiler::class);
+        $this->profiler = $this->container->get(ProfilerInterface::class);
         $this->connection = $this->container->get(ConnectionInterface::class);
         $this->queryCache = $this->container->get(QueryCache::class);
         $this->schemaCache = $this->container->get(SchemaCache::class);
@@ -284,6 +285,7 @@ class TestCase extends AbstractTestCase
             ],
 
             LoggerInterface::class => Logger::class,
+            ProfilerInterface::class => Profiler::class,
 
             ConnectionInterface::class => [
                 '__class' => Connection::class,
