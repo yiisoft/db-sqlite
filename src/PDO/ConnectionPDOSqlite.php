25a1f54f8e7fc7f88b20b24e456c2bfca79bcb39
darkdef <darkdef@gmail.com>
2022-04-14 22:51:47 +0300

    Common constructor for ConnectionPDO

--- src/PDO/ConnectionPDOSqlite.php
+++ src/PDO/ConnectionPDOSqlite.php
@@ -5,11 +5,8 @@ declare(strict_types=1);
 namespace Yiisoft\Db\Sqlite\PDO;
 
 use PDO;
-use Yiisoft\Db\Cache\QueryCache;
-use Yiisoft\Db\Cache\SchemaCache;
 use Yiisoft\Db\Command\CommandInterface;
 use Yiisoft\Db\Connection\ConnectionPDO;
-use Yiisoft\Db\Driver\PDODriver;
 use Yiisoft\Db\Exception\Exception;
 use Yiisoft\Db\Exception\InvalidConfigException;
 use Yiisoft\Db\Query\QueryBuilderInterface;
@@ -25,14 +22,6 @@ use function constant;
  */
 final class ConnectionPDOSqlite extends ConnectionPDO
 {
-    public function __construct(
-        protected PDODriver $driver,
-        protected QueryCache $queryCache,
-        protected SchemaCache $schemaCache
-    ) {
-        parent::__construct($queryCache);
-    }
-
     public function createCommand(?string $sql = null, array $params = []): CommandInterface
     {
         $command = new CommandPDOSqlite($this, $this->queryCache);
