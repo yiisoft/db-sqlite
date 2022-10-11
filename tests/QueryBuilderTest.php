230e175e0590d1f86abbe88da5a4718622686190
Wilmer Arambula <terabytesoftw@gmail.com>
2022-10-11 10:07:40 -0300

    Fixed unit tests.

--- tests/QueryBuilderTest.php
+++ tests/QueryBuilderTest.php
@@ -54,7 +54,7 @@ final class QueryBuilderTest extends TestCase
      *
      * @throws Exception|InvalidArgumentException|InvalidConfigException|NotSupportedException
      */
-    public function testBuildCondition(array|ExpressionInterface $condition, string $expected, array $expectedParams): void
+    public function testBuildCondition(array|ExpressionInterface|string $condition, string $expected, array $expectedParams): void
     {
         $db = $this->getConnection();
         $query = (new Query($db))->where($condition);
