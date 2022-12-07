6bd568cd8c0d0c1663618f86feb45a90398c43da
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2022-12-07 10:25:35 -0300

    Simplify CommandProvider::class. (#150)
    * Use inheritance.

--- tests/Provider/CommandProvider.php
+++ tests/Provider/CommandProvider.php
@@ -4,59 +4,18 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Sqlite\Tests\Provider;
 
-use Yiisoft\Db\Exception\Exception;
-use Yiisoft\Db\Exception\InvalidConfigException;
 use Yiisoft\Db\Sqlite\Tests\Support\TestTrait;
-use Yiisoft\Db\Tests\Provider\BaseCommandProvider;
+use Yiisoft\Db\Tests\Provider\AbstractCommandProvider;
 
-final class CommandProvider
+final class CommandProvider extends AbstractCommandProvider
 {
     use TestTrait;
 
-    /**
-     * @throws Exception
-     * @throws InvalidConfigException
-     */
     public function batchInsert(): array
     {
-        $baseCommandProvider = new BaseCommandProvider();
-
-        $batchInsertSql = $baseCommandProvider->batchInsert($this->getConnection());
+        $batchInsertSql = parent::batchInsert();
         unset($batchInsertSql['wrongBehavior']);
 
         return $batchInsertSql;
-    }
-
-    /**
-     * @throws Exception
-     * @throws InvalidConfigException
-     */
-    public function rawSql(): array
-    {
-        $baseCommandProvider = new BaseCommandProvider();
-
-        return $baseCommandProvider->rawSql($this->getConnection());
-    }
-
-    /**
-     * @throws Exception
-     * @throws InvalidConfigException
-     */
-    public function update(): array
-    {
-        $baseCommandProvider = new BaseCommandProvider();
-
-        return $baseCommandProvider->update($this->getConnection());
-    }
-
-    /**
-     * @throws Exception
-     * @throws InvalidConfigException
-     */
-    public function upsert(): array
-    {
-        $baseCommandProvider = new BaseCommandProvider();
-
-        return $baseCommandProvider->upsert($this->getConnection());
     }
 }
