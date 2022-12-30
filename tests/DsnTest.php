cb94a40cbb67768a0e70ad316fbaad1e6149226c
Wilmer Arambula <terabytesoftw@gmail.com>
2022-12-30 11:53:00 -0300

    Improvements `Dsn::class`, fix phpdoc, add more tests.

---
+++ tests/DsnTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Db\Sqlite\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Db\Sqlite\Dsn;
+
+/**
+ * @group sqlite
+ *
+ * @psalm-suppress PropertyNotSetInConstructor
+ */
+final class DsnTest extends TestCase
+{
+    public function testAsString(): void
+    {
+        $this->assertSame(
+            'sqlite:' . __DIR__ . '/runtime/yiitest.sq3',
+            (new Dsn('sqlite', __DIR__ . '/runtime/yiitest.sq3'))->asString(),
+        );
+    }
+
+    public function testAsStringWithEmpty(): void
+    {
+        $this->assertSame(
+            'sqlite:',
+            (new Dsn('sqlite'))->asString(),
+        );
+    }
+
+    public function testAsStringWithMemory(): void
+    {
+        $this->assertSame(
+            'sqlite::memory:',
+            (new Dsn('sqlite', 'memory'))->asString(),
+        );
+    }
+}
--- tests/Support/TestTrait.php
+++ tests/Support/TestTrait.php
@@ -8,12 +8,13 @@ use Yiisoft\Db\Driver\PDO\ConnectionPDOInterface;
 use Yiisoft\Db\Exception\Exception;
 use Yiisoft\Db\Exception\InvalidConfigException;
 use Yiisoft\Db\Sqlite\ConnectionPDO;
+use Yiisoft\Db\Sqlite\Dsn;
 use Yiisoft\Db\Sqlite\PDODriver;
 use Yiisoft\Db\Tests\Support\DbHelper;
 
 trait TestTrait
 {
-    private string $dsn = 'sqlite::memory:';
+    private string $dsn = '';
 
     /**
      * @throws Exception
@@ -21,15 +22,26 @@ trait TestTrait
      */
     protected function getConnection(bool $fixture = false): ConnectionPDOInterface
     {
-        $pdoDriver = new PDODriver($this->dsn);
-
-        $db = new ConnectionPDO($pdoDriver, DbHelper::getQueryCache(), DbHelper::getSchemaCache());
+        $db = new ConnectionPDO(
+            new PDODriver($this->getDsn()),
+            DbHelper::getQueryCache(),
+            DbHelper::getSchemaCache(),
+        );
 
         if ($fixture) {
             DbHelper::loadFixture($db, __DIR__ . '/Fixture/sqlite.sql');
         }
 
         return $db;
+    }
+
+    protected function getDsn(): string
+    {
+        if ($this->dsn === '') {
+            $this->dsn = (new Dsn('sqlite', 'memory'))->asString();
+        }
+
+        return $this->dsn;
     }
 
     protected function getDriverName(): string
