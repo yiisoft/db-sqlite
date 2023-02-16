587a51bd3306d798f6f2880609591410be1cbee3
Wilmer Arambula <terabytesoftw@gmail.com>
2023-02-16 12:33:30 -0300

    Update phpunit 10 - part 3.

--- tests/Provider/CommandProvider.php
+++ tests/Provider/CommandProvider.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 namespace Yiisoft\Db\Sqlite\Tests\Provider;
 
 use Yiisoft\Db\Sqlite\Tests\Support\TestTrait;
-use Yiisoft\Db\Tests\Provider\AbstractCommandProvider;
 
-final class CommandProvider extends AbstractCommandProvider
+final class CommandProvider extends \Yiisoft\Db\Tests\Provider\CommandProvider
 {
     use TestTrait;
 
