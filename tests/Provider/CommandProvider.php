7d342902c0115de9eabaf7d2ff8342cbf16ae7a3
Wilmer Arambula <terabytesoftw@gmail.com>
2023-02-16 20:14:47 -0300

    Change methods to static `CommandProvider::class`.

--- tests/Provider/CommandProvider.php
+++ tests/Provider/CommandProvider.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Sqlite\Tests\Provider;
 
-use Yiisoft\Db\Sqlite\Tests\Support\TestTrait;
-
 final class CommandProvider extends \Yiisoft\Db\Tests\Provider\CommandProvider
 {
-    use TestTrait;
-
-    public function batchInsert(): array
+    public static function batchInsert(): array
     {
         return parent::batchInsert();
     }
