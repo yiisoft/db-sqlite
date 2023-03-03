a2c043cf92088ba9f5e0801e0607639208a3e7f5
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-03-03 19:39:58 -0300

    Clean code `LikeConditionBuilder::class`. (#212)

--- src/Builder/LikeConditionBuilder.php
+++ src/Builder/LikeConditionBuilder.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Sqlite\Builder;
 
-use Yiisoft\Db\QueryBuilder\Condition\Builder\LikeConditionBuilder as BaseLikeConditionBuilder;
 use Yiisoft\Db\QueryBuilder\QueryBuilderInterface;
 
-final class LikeConditionBuilder extends BaseLikeConditionBuilder
+/**
+ * Builds conditions for {@see `\Yiisoft\Db\QueryBuilder\Condition\LikeCondition`} LIKE operator
+ * for SQLite Server.
+ */
+final class LikeConditionBuilder extends \Yiisoft\Db\QueryBuilder\Condition\Builder\LikeConditionBuilder
 {
     private string $escapeCharacter = '\\';
 
@@ -17,12 +20,11 @@ final class LikeConditionBuilder extends BaseLikeConditionBuilder
     }
 
     /**
-     * @return string character used to escape special characters in LIKE conditions.
-     *
-     * By default, it's assumed to be `\`.
+     * @return string character used to escape special characters in LIKE conditions. By default, it's assumed to be
+     * `\`.
      */
     private function getEscapeSql(): string
     {
-        return $this->escapeCharacter !== '' ? " ESCAPE '{$this->escapeCharacter}'" : '';
+        return $this->escapeCharacter !== '' ? " ESCAPE '$this->escapeCharacter'" : '';
     }
 }
