d1afb735074b2faf3e00aef69575779e02d648d2
Wilmer Arambula <terabytesoftw@gmail.com>
2023-03-05 07:10:36 -0300

    Add phpdoc `ColumnSchema` class.

--- src/ColumnSchema.php
+++ src/ColumnSchema.php
@@ -6,6 +6,29 @@ namespace Yiisoft\Db\Sqlite;
 
 use Yiisoft\Db\Schema\AbstractColumnSchema;
 
+/**
+ * Represents the metadata of a column in a database table for SQLite Server. It provides information about the column's
+ * name, type, size, precision, and other details.
+ *
+ * Is used to store and retrieve metadata about a column in a database table. It is typically used in conjunction with
+ * the {@see TableSchema}, which represents the metadata of a database table as a whole.
+ *
+ * The following code shows how to use ColumnSchema:
+ *
+ * ```php
+ * use Yiisoft\Db\Sqlite\ColumnSchema;
+ *
+ * $column = new ColumnSchema();
+ * $column->name('id');
+ * $column->allowNull(false);
+ * $column->dbType('integer');
+ * $column->phpType('integer');
+ * $column->type('integer');
+ * $column->defaultValue(0);
+ * $column->autoIncrement(true);
+ * $column->primaryKey(true);
+ * ```
+ */
 final class ColumnSchema extends AbstractColumnSchema
 {
 }
