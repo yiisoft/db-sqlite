e073cfe00b3dbd99c222292c0547d917b00e3588
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-03-29 17:20:49 -0300

    Add phpdoc constans `AbstractColumn::class`. (#232)

--- src/Column.php
+++ src/Column.php
@@ -36,10 +36,10 @@ final class Column extends AbstractColumn
     public function asString(): string
     {
         $format = match ($this->getTypeCategory()) {
-            self::CATEGORY_PK => '{type}{check}{append}',
-            self::CATEGORY_NUMERIC => '{type}{length}{unsigned}{notnull}{unique}{check}{default}{append}',
-            self::CATEGORY_UUID => '{type}{notnull}{unique}{default}{check}{comment}{append}',
-            self::CATEGORY_UUID_PK => '{type}{notnull}{default}{check}{comment}{append}',
+            self::TYPE_CATEGORY_PK => '{type}{check}{append}',
+            self::TYPE_CATEGORY_NUMERIC => '{type}{length}{unsigned}{notnull}{unique}{check}{default}{append}',
+            self::TYPE_CATEGORY_UUID => '{type}{notnull}{unique}{default}{check}{comment}{append}',
+            self::TYPE_CATEGORY_UUID_PK => '{type}{notnull}{default}{check}{comment}{append}',
             default => '{type}{length}{notnull}{unique}{check}{default}{append}',
         };
 
