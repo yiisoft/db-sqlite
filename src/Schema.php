9edbb8ef010703026a32089224c6699f99749ee0
Wilmer Arambula <terabytesoftw@gmail.com>
2023-04-04 14:40:10 -0400

    Remove `getCacheKey()` to `ConnectionInterface::class`.

--- src/Schema.php
+++ src/Schema.php
@@ -9,13 +9,13 @@ use Yiisoft\Db\Constraint\CheckConstraint;
 use Yiisoft\Db\Constraint\Constraint;
 use Yiisoft\Db\Constraint\ForeignKeyConstraint;
 use Yiisoft\Db\Constraint\IndexConstraint;
+use Yiisoft\Db\Driver\PDO\PdoAbstractSchema;
 use Yiisoft\Db\Exception\Exception;
 use Yiisoft\Db\Exception\InvalidArgumentException;
 use Yiisoft\Db\Exception\InvalidConfigException;
 use Yiisoft\Db\Exception\NotSupportedException;
 use Yiisoft\Db\Expression\Expression;
 use Yiisoft\Db\Helper\ArrayHelper;
-use Yiisoft\Db\Schema\AbstractSchema;
 use Yiisoft\Db\Schema\Builder\ColumnInterface;
 use Yiisoft\Db\Schema\ColumnSchemaInterface;
 use Yiisoft\Db\Schema\TableSchemaInterface;
@@ -73,7 +73,7 @@ use function trim;
  *   array{cid:string, name:string, type:string, notnull:string, dflt_value:string|null, pk:string}
  * >
  */
-final class Schema extends AbstractSchema
+final class Schema extends PdoAbstractSchema
 {
     /**
      * @var array Mapping from physical column types (keys) to abstract column types (values).
@@ -695,7 +695,7 @@ final class Schema extends AbstractSchema
      */
     protected function getCacheKey(string $name): array
     {
-        return array_merge([self::class], $this->db->getCacheKey(), [$this->getRawTableName($name)]);
+        return array_merge([self::class], $this->generateCacheKey(), [$this->getRawTableName($name)]);
     }
 
     /**
@@ -707,6 +707,6 @@ final class Schema extends AbstractSchema
      */
     protected function getCacheTag(): string
     {
-        return md5(serialize(array_merge([self::class], $this->db->getCacheKey())));
+        return md5(serialize(array_merge([self::class], $this->generateCacheKey())));
     }
 }
--- tests/SchemaTest.php
+++ tests/SchemaTest.php
@@ -6,10 +6,12 @@ namespace Yiisoft\Db\Sqlite\Tests;
 
 use JsonException;
 use Throwable;
+use Yiisoft\Db\Connection\ConnectionInterface;
 use Yiisoft\Db\Constraint\CheckConstraint;
 use Yiisoft\Db\Exception\Exception;
 use Yiisoft\Db\Exception\InvalidConfigException;
 use Yiisoft\Db\Exception\NotSupportedException;
+use Yiisoft\Db\Sqlite\Schema;
 use Yiisoft\Db\Sqlite\Tests\Support\TestTrait;
 use Yiisoft\Db\Tests\Common\CommonSchemaTest;
 
@@ -314,5 +316,16 @@ final class SchemaTest extends CommonSchemaTest
         );
 
         parent::testWorkWithPrimaryKeyConstraint();
+    }
+
+    public function testNotConnectionPDO(): void
+    {
+        $db = $this->createMock(ConnectionInterface::class);
+        $schema = new Schema($db, DbHelper::getSchemaCache(), 'system');
+
+        $this->expectException(NotSupportedException::class);
+        $this->expectExceptionMessage('Only PDO connections are supported.');
+
+        $schema->refreshTableSchema('customer');
     }
 }
