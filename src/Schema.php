1b7220630b007e9f583a4ac4381d8988d9d3d97a
Tigrov <rrr-r@ya.ru>
2023-07-04 23:28:35 +0700

    Typecast refactoring

--- src/Schema.php
+++ src/Schema.php
@@ -493,19 +493,36 @@ final class Schema extends AbstractPdoSchema
         }
 
         $column->phpType($this->getColumnPhpType($column));
-
-        if (!$column->isPrimaryKey()) {
-            if ($info['dflt_value'] === 'null' || $info['dflt_value'] === '' || $info['dflt_value'] === null) {
-                $column->defaultValue(null);
-            } elseif ($info['dflt_value'] === 'CURRENT_TIMESTAMP' && $column->getType() === 'timestamp') {
-                $column->defaultValue(new Expression('CURRENT_TIMESTAMP'));
-            } else {
-                $value = trim($info['dflt_value'], "'\"");
-                $column->defaultValue($column->phpTypecast($value));
-            }
-        }
+        $column->defaultValue($this->normalizeDefaultValue($info['dflt_value'], $column));
 
         return $column;
+    }
+
+    /**
+     * Converts column's default value according to {@see ColumnSchema::phpType} after retrieval from the database.
+     *
+     * @param string|null $defaultValue The default value retrieved from the database.
+     * @param ColumnSchemaInterface $columnSchema The column schema object.
+     *
+     * @return mixed The normalized default value.
+     */
+    private function normalizeDefaultValue(?string $defaultValue, ColumnSchemaInterface $columnSchema): mixed
+    {
+        return match (true) {
+            $defaultValue === null,
+            $defaultValue === 'null',
+            $defaultValue === '',
+            $columnSchema->isPrimaryKey()
+                => null,
+            in_array($defaultValue, [
+                    'CURRENT_TIMESTAMP',
+                    'CURRENT_DATE',
+                    'CURRENT_TIME'
+                ],true)
+                    => new Expression($defaultValue),
+            default
+                => $columnSchema->phpTypecast(trim($defaultValue, "'\"")),
+        };
     }
 
     /**
