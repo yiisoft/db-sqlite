24895e1f110711a3e61b89f7c5869e33a5851e56
Tigrov <rrr-r@ya.ru>
2024-08-14 10:59:31 +0700

    Fix psalm issue

--- src/Column/ColumnFactory.php
+++ src/Column/ColumnFactory.php
@@ -12,6 +12,8 @@ class ColumnFactory extends \Yiisoft\Db\Schema\Column\ColumnFactory
      * Mapping from physical column types (keys) to abstract column types (values).
      *
      * @var string[]
+     *
+     * @psalm-suppress MissingClassConstType
      */
     private const TYPE_MAP = [
         'bool' => SchemaInterface::TYPE_BOOLEAN,
