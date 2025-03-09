c6b2255605e0d9badc5124a7d2bb0c8be1d7d854
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-03-09 20:33:55 +0300

    improve

--- src/Command.php
+++ src/Command.php
@@ -8,7 +8,6 @@ use Throwable;
 use Yiisoft\Db\Driver\Pdo\AbstractPdoCommand;
 use Yiisoft\Db\Exception\Exception;
 use Yiisoft\Db\Exception\InvalidArgumentException;
-use Yiisoft\Db\Exception\NotSupportedException;
 
 use function array_pop;
 use function count;
@@ -22,17 +21,6 @@ use function strpos;
  */
 final class Command extends AbstractPdoCommand
 {
-    /**
-     * @throws NotSupportedException SQLite doesn't support cascade drop table.
-     */
-    public function dropTable(string $table, bool $ifExists = false, bool $cascade = false): static
-    {
-        if ($cascade) {
-            throw new NotSupportedException('SQLite doesn\'t support cascade drop table.');
-        }
-        return parent::dropTable($table, $ifExists, false);
-    }
-
     public function insertWithReturningPks(string $table, array $columns): bool|array
     {
         $params = [];
