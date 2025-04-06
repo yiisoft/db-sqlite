7c75b7b06fd67dcea513a5e08baf8545cbd2469e
Sergei Tigrov <rrr-r@ya.ru>
2025-04-06 23:37:40 +0700

    Call `DbArrayHelper::arrange()` instead of `DbArrayHelper::index()` (#350)

--- src/Schema.php
+++ src/Schema.php
@@ -158,7 +158,7 @@ final class Schema extends AbstractPdoSchema
 
         $foreignKeysList = $this->getPragmaForeignKeyList($tableName);
         /** @psalm-var GroupedForeignKeyInfo $foreignKeysList */
-        $foreignKeysList = DbArrayHelper::index($foreignKeysList, null, ['table', 'id']);
+        $foreignKeysList = DbArrayHelper::arrange($foreignKeysList, ['table', 'id']);
 
         foreach ($foreignKeysList as $table => $foreignKeysById) {
             /**
@@ -453,11 +453,8 @@ final class Schema extends AbstractPdoSchema
     private function loadTableColumnsInfo(string $tableName): array
     {
         $tableColumns = $this->getPragmaTableInfo($tableName);
-        /** @psalm-var ColumnInfo[] $tableColumns */
-        $tableColumns = array_map(array_change_key_case(...), $tableColumns);
-
         /** @psalm-var ColumnInfo[] */
-        return DbArrayHelper::index($tableColumns, 'cid');
+        return array_map(array_change_key_case(...), $tableColumns);
     }
 
     /**
