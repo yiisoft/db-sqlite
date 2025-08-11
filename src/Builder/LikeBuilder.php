66183d5abf4886e1efabcb4666f1034f930af197
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-08-11 14:39:30 +0300

    Adapt to split Like condition in Yii DB (#382)

--- src/Builder/LikeBuilder.php
+++ src/Builder/LikeBuilder.php
@@ -6,19 +6,13 @@ namespace Yiisoft\Db\Sqlite\Builder;
 
 use Yiisoft\Db\Exception\NotSupportedException;
 use Yiisoft\Db\Expression\ExpressionInterface;
-use Yiisoft\Db\QueryBuilder\QueryBuilderInterface;
 
 /**
  * Build an object of {@see \Yiisoft\Db\QueryBuilder\Condition\LikeCondition} into SQL expressions for SQLite Server.
  */
 final class LikeBuilder extends \Yiisoft\Db\QueryBuilder\Condition\Builder\LikeBuilder
 {
-    private string $escapeCharacter = '\\';
-
-    public function __construct(QueryBuilderInterface $queryBuilder)
-    {
-        parent::__construct($queryBuilder, $this->getEscapeSql());
-    }
+    protected const ESCAPE_SQL = " ESCAPE '\\'";
 
     public function build(ExpressionInterface $expression, array &$params = []): string
     {
@@ -26,14 +20,5 @@ final class LikeBuilder extends \Yiisoft\Db\QueryBuilder\Condition\Builder\LikeB
             throw new NotSupportedException('SQLite doesn\'t support case-sensitive "LIKE" conditions.');
         }
         return parent::build($expression, $params);
-    }
-
-    /**
-     * @return string Character used to escape special characters in `LIKE` conditions.
-     * By default, it's assumed to be `\`.
-     */
-    private function getEscapeSql(): string
-    {
-        return $this->escapeCharacter !== '' ? " ESCAPE '$this->escapeCharacter'" : '';
     }
 }
