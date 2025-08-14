33b793a71ff3c788c49c21a2e98ad4a5611c2e07
Tigrov <8563175+Tigrov@users.noreply.github.com>
2025-08-14 19:41:37 +0000

    Apply Rector changes (CI)

--- tests/Provider/QueryBuilderProvider.php
+++ tests/Provider/QueryBuilderProvider.php
@@ -426,7 +426,7 @@ final class QueryBuilderProvider extends \Yiisoft\Db\Tests\Provider\QueryBuilder
 
     public static function multiOperandFunctionBuilder(): array
     {
-        $data = parent::multiOperandFunctionBuilder();
+        $data = null;
 
         $intQuery = self::getDb()->select(10);
         $intQuerySql = '(SELECT 10)';
