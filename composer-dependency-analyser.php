3074c62c1105ebc47eceecd1f2afc189b736197b
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-08 08:20:40 +0300

    Add Composer Dependency Analyser

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    ->disableComposerAutoloadPathScan()
+    ->setFileExtensions(['php'])
+    ->addPathToScan(__DIR__ . '/src', isDev: false)
+    ->addPathToScan(__DIR__ . '/tests', isDev: true)
+    // Used only implicitly via the `sqlite:` PDO DSN, no statically-detectable symbol.
+    ->ignoreErrorsOnExtension('ext-pdo_sqlite', [ErrorType::UNUSED_DEPENDENCY]);
